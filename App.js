0c13e8480f99490c79eb14a3a66bb79f2b1c84ce
michael071788 <michael071788@gmail.com>
2022-07-19 20:41:23 +0400

    feat :tada: initial image data

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import "./ignoreWarnings.js";
 import React, { useEffect, useState } from "react";
 import {
+  Button,
   Dimensions,
+  Image,
   Platform,
   SafeAreaView,
   StatusBar,
@@ -12,6 +14,8 @@ import {
 import Carousel from "react-native-snap-carousel";
 import { scrollInterpolator, animatedStyles } from "./src/utils/animations";
 // import styles from "./src/styles/index.style";
+import * as VideoThumbnails from "expo-video-thumbnails";
+
 const { width: viewportWidth, height: viewportHeight } =
   Dimensions.get("window");
 
@@ -37,7 +41,10 @@ const _renderItem = ({ item }) => {
   return (
     <View style={styles.slide}>
       <View style={styles.slideInner}>
-        <Text>{`Item ${item}`}</Text>
+        <Image
+          style={{ flex: 1, borderRadius: 10 }}
+          source={{ uri: item.imageURI }}
+        />
       </View>
     </View>
   );
@@ -48,23 +55,30 @@ export default function App() {
     <View
       style={{
         flex: 1,
-        backgroundColor: "#e0e0e0",
+        backgroundColor: "white",
         flexDirection: "column",
         alignItems: "stretch",
       }}
     >
-      <View style={{ height: wp(35), backgroundColor: "#fa6b6b" }} />
+      <View
+        style={{
+          height: wp(35),
+          // backgroundColor: "#fa6b6b",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      />
       <View
         style={{
           flex: 3,
-          backgroundColor: "#f78d8d",
+          // backgroundColor: "#f78d8d",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
         <Carousel
           // ref={(c) => (this.carousel = c)}
-          data={DATA}
+          data={ENTRIES1}
           renderItem={_renderItem}
           sliderWidth={sliderWidth}
           sliderHeight={slideHeight}
@@ -79,7 +93,12 @@ export default function App() {
           vertical={true}
         />
       </View>
-      <View style={{ height: wp(20), backgroundColor: "#fa6b6b" }} />
+      <View
+        style={{
+          height: wp(20),
+          //backgroundColor: "#fa6b6b"
+        }}
+      />
     </View>
   );
 }
@@ -93,7 +112,42 @@ const styles = StyleSheet.create({
     height: slideHeight,
     paddingHorizontal: itemHorizontalMargin,
     paddingVertical: itemVerticalMargin,
-    backgroundColor: "dodgerblue",
+    // backgroundColor: "dodgerblue",
   },
-  slideInner: { flex: 1, backgroundColor: "#6e7cf5" },
+  slideInner: {
+    flex: 1, //backgroundColor: "#6e7cf5",
+    borderRadius: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
 });
+
+const ENTRIES1 = [
+  {
+    id: 1,
+    imageURI:
+      "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+  },
+  {
+    id: 2,
+    imageURI:
+      "https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441",
+  },
+  {
+    id: 3,
+    imageURI:
+      "https://s-i.huffpost.com/gen/1317262/images/o-ANNA-GUNN-facebook.jpg",
+  },
+  {
+    id: 4,
+    imageURI:
+      "https://media1.popsugar-assets.com/files/thumbor/WeLUSvbAMS_GL4iELYAUzu7Bpv0/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/01/12/910/n/1922283/fb758e62b5daf3c9_TCDBRBA_EC011/i/RJ-Mitte-Walter-White-Jr.jpg",
+  },
+  {
+    id: 5,
+    imageURI:
+      "https://vignette.wikia.nocookie.net/breakingbad/images/b/b7/HankS5.jpg/revision/latest/scale-to-width-down/700?cb=20120620014136",
+  },
+];
